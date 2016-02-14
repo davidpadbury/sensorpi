db5aa2849dafff1ab353795978c17d663b267b58
David Padbury <david@davidpadbury.com>
2016-02-14 12:21:10 -0600

    log readings to statsd

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const lightSensor = require('./lib/lightSensor')
 const temperatureSensor = require('./lib/temperatureSensor')
+const statsd = new (require('node-statsd'))()
 
 const readLight = lightSensor()
 const readTemperature = temperatureSensor(20)
 
-setInterval(() => console.log(readLight()), 500)
-setInterval(() => console.log(readTemperature()), 2000)
+setInterval(logLight, 500)
+setInterval(logTemperature, 2000)
+
+function logLight () {
+  const reading = readLight()
+  statsd.gauge('ir', reading.ir)
+  statsd.gauge('full', reading.full)
+}
+
+function logTemperature () {
+  const reading = readTemperature()
+  statsd.gauge('humidity', reading.humidity)
+  statsd.gauge('temperature', reading.temperature)
+}
