8fcce580fbf2b0c2c96b7a8e0f8d44a18ceea297
David Padbury <david@davidpadbury.com>
2016-02-13 18:33:23 -0600

    temperature sensing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const lightSensor = require('./lib/lightSensor')
+const temperatureSensor = require('./lib/temperatureSensor')
 
 const readLight = lightSensor()
+const readTemperature = temperatureSensor(20)
 
 setInterval(() => console.log(readLight()), 500)
+setInterval(() => console.log(readTemperature()), 2000)
